b774506ce7a4401eeff9a19a1205faa68a5dd535
Matt Todd <chiology@gmail.com>
2009-01-09 04:40:16 -0500

    Added minor documentation.

--- fuck.rb
+++ fuck.rb
@@ -1,5 +1,64 @@
 require 'rack'
 
+# Fuck is a fuckin' framework.
+# 
+# It's:
+# * kinda fucking retarded
+# * hastily done
+# * rather pointless
+# * far from thorough or done
+# 
+# Fuck::Resource apps are able to be recognized and called on. The class name
+# will match up to the request, and the action is determined by the request
+# method.
+# 
+# There is a middleware for emulating the proper request method if you're using
+# forms and are not able to properly PUT and DELETE.
+# 
+# The ID is what trails the resource's name, by a slash.
+# 
+# To illustrate:
+#   GET     /posts    => Posts#all
+#   POST    /posts    => Posts#create
+#   GET     /posts/1  => Posts#read(id)
+#   PUT     /posts/1  => Posts#update(id)
+#   DELETE  /posts/1  => Posts#delete(id)
+# 
+# Usage:
+# 
+#   class Posts < Fuck::Resource
+#     
+#     # GET /posts
+#     def all
+#       respond("OK", :status => 200)
+#     end
+#     
+#     # POST /posts/1
+#     def create(id)
+#       respond("OK", :status => 200)
+#     end
+#     
+#     # GET /posts/1
+#     def read(id)
+#       respond("OK", :status => 200)
+#     end
+#     
+#     # PUT /posts/1
+#     def update(id)
+#       respond("OK", :status => 200)
+#     end
+#     
+#     # DELETE /posts/1
+#     def delete(id)
+#       respond("OK", :status => 200)
+#     end
+#     
+#   end
+# 
+# Obviously, you'll want to do a little bit of work with actually getting the
+# contents of an object and displaying it correctly. This is left (for now)
+# entirely up to you.
+# 
 class Fuck
   class << self
     
