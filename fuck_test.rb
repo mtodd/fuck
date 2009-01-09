9de42e6ee821ee5bbebc1b22c254185e87bba4fb
Matt Todd <chiology@gmail.com>
2009-01-09 03:19:46 -0500

    Minor improvements to testing, along with some additional testing resources.

--- fuck_test.rb
+++ fuck_test.rb
@@ -27,45 +27,66 @@ class Posts < Fuck::Resource
   end
 end
 
+class Exceptions < Fuck::Resource
+  def all
+    raise "An error in the app"
+  end
+  # def create
+  #   # make it not exist
+  # end
+  def read(id)
+    # do not respond with anything
+  end
+  def update(id)
+    [200, {"Content-Type"=>"text/html"}, "OK"]
+  end
+  def delete(id)
+    # TBD
+  end
+end
+
 ### Tests
+
+def request(path, options = {})
+  Fuck.call(Rack::MockRequest.env_for(path, options))
+end
+def get(path, options = {}) request(path, options.merge({"REQUEST_METHOD" => "GET"})) end
+def post(path, options = {}) request(path, options.merge({"REQUEST_METHOD" => "POST"})) end
+def put(path, options = {}) request(path, options.merge({"REQUEST_METHOD" => "PUT"})) end
+def delete(path, options = {}) request(path, options.merge({"REQUEST_METHOD" => "DELETE"})) end
 
 context "Fuck" do
   
   specify "can route to list all of the resources" do
-    env = Rack::MockRequest.env_for("/posts")
-    status, headers, body = Fuck.call(env)
+    status, headers, body = get("/posts")
     
     status.should == 200
     body.should =~ /Fuckin A!/
   end
   
   specify "can route to read the specific resource" do
-    env = Rack::MockRequest.env_for("/posts/1")
-    status, headers, body = Fuck.call(env)
+    status, headers, body = get("/posts/1")
     
     status.should == 200
     body.should =~ /You asked for 1/
   end
   
   specify "can route to delete the specific resource" do
-    env = Rack::MockRequest.env_for("/posts/1", "REQUEST_METHOD" => "DELETE")
-    status, headers, body = Fuck.call(env)
+    status, headers, body = delete("/posts/1")
     
     status.should == 200
     body.should =~ /You asked me to delete 1/
   end
   
   specify "can route to create a new resource" do
-    env = Rack::MockRequest.env_for("/posts?a=b", "REQUEST_METHOD" => "PUT")
-    status, headers, body = Fuck.call(env)
+    status, headers, body = put("/posts?a=b")
     
     status.should == 200
     body.should =~ /b/
   end
   
   specify "can route updates for a given resource" do
-    env = Rack::MockRequest.env_for("/posts/1?a=c", "REQUEST_METHOD" => "POST")
-    status, headers, body = Fuck.call(env)
+    status, headers, body = post("/posts/1?a=c")
     
     status.should == 200
     body.should =~ /c/
